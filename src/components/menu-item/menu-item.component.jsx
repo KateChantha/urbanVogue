963fb14edea4582475e528f2d0c1603954689827
Kato <kubkate@gmail.com>
2020-08-05 16:30:25 -0700

    In App.js,add Routes for Homepage and HatsPage-> MenuItem to navigate w/ withRouter()

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.scss';
 
 /**
  * dynamicly generate title and image that passing thru props
  * destructuring {tilte} instead of writing props.tilte
  */
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`} >
-    <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      className='background-image'
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    />
     <div className='content'>
       <h1 className='title'>{title.toUpperCase()}</h1>
       <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>
-)
+);
 
-export default MenuItem;   
+// pass MenuItem to withRouter which is a HOC-Higher Order Component/Function so that MenuItem able to gain access to anything realated to router such as history and match
+// withRouter(MenuItem)returns super power MenuItem
+export default withRouter(MenuItem);   
