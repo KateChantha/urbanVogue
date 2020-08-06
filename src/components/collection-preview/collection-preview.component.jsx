07564aa3389d8face60e3d05da79fb5b88bff314
Kato <kubkate@gmail.com>
2020-08-05 19:22:19 -0700

    Update CollectionPreview logic- filter limit at 4 items

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -6,15 +6,20 @@ import './collection-preview.scss';
  * @param title string
  * @param items array
  * map over items array
+ * filter out limit to 4 items
+ * TODO: Perfromance concern due to everytime CollectionPreview got rerender, items.filer().map() will get call again and they have to be rerendered
+ * TODO: logic filter out item, should be 4 hot feed
  */
 const CollectionPreview = ({ title, items }) =>
   (
     <div className='collection-preview'>
       <h1 className='title'>{title.toUpperCase()}</h1>
       <div className='preview'>
-        {items.map(item => (
-          <div key={item.id}>{item.name}</div>
-        ))}
+        {items
+          .filter((item, idx) => idx < 4)
+          .map(item => (
+            <div key={item.id}>{item.name}</div>
+          ))}
       </div>
     </div>
   )
