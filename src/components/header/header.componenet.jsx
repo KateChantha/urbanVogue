5c62167bf23e05eff2c563ddf6ea0007b9e70808
Kato <kubkate@gmail.com>
2020-08-06 13:06:35 -0700

    modify style of <a> in App.scss for <Link>

--- src/components/header/header.componenet.jsx
+++ src/components/header/header.componenet.jsx
@@ -6,6 +6,9 @@ import { ReactComponent as Logo } from '../../assets/crown.svg'
 
 import './header.scss';
 
+//==== NOTE: <Link /> is native <a> in DOM =====
+// <a class="option" href="/shop">SHOP<a/>
+
 const Header = () => (
   <div className='header'>
     <Link className='logo-container' to="/">
