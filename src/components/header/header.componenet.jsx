656a329e619a9873febe7cbfcba7fa724e2cf347
Kato <kubkate@gmail.com>
2020-08-07 13:21:10 -0700

    In Header, render SIGNIN SIGNOUT depends on state of currentUser

--- src/components/header/header.componenet.jsx
+++ src/components/header/header.componenet.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
+import { auth } from '../../firebase/firebase.utils';
+
 // special syntax in React to import .svg
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
@@ -9,7 +11,7 @@ import './header.scss';
 //==== NOTE: <Link /> is native <a> in DOM =====
 // <a class="option" href="/shop">SHOP<a/>
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className='header'>
     <Link className='logo-container' to="/">
       <Logo className='logo' />
@@ -21,6 +23,12 @@ const Header = () => (
       <Link className='option' to="/contact">
         CONTACT
       </Link>
+      {
+        currentUser ?
+          <div className='option' onClick={() => auth.signOut()} >SIGN OUT</div>
+          :
+          <Link className='option' to='/signin' >SIGN IN</Link>
+      }
     </div>
   </div>
 )
