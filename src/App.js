0c343512129f0f5cab0acb9aac79bb507aa4b43c
Kato <kubkate@gmail.com>
2020-08-07 19:11:51 -0700

    In App.js, add logic to setState depends on user data from db when signin sign out

--- src/App.js
+++ src/App.js
@@ -31,12 +31,32 @@ class App extends React.Component {
   componentDidMount() {
     // this connection always open as long as app mouted on the DOM. so, no need to keep fecthing to check if user status has changed.
     // auth.onAuthStateChanged() returns fucntion store in unsubscribeFromAuth method/property
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      // this.setState({ currentUser: user })
-      // console.log(user)
-      createUserProfileDocument(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      // if userAuth is exist/or sign-in
+      if (userAuth) {
+        // call createUserProfileDocument()-in firebase.utils, will first check it user exist 
+        // then return userRef and assign to userRef here
+        const userRef = await createUserProfileDocument(userAuth);
 
-    })
+        // get back the first state of user's data and keep subscribe/ listen to userRef for any changes to that data 
+        userRef.onSnapshot(snapShot => {
+          // have to call with .data() to get all other properties that store in db
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              // get the rest of other/deeper data by calling .data() on snapshot obj
+              ...snapShot.data()
+            }
+          }, () => {
+            // optional to check data
+            console.log("this.state: ", this.state)
+          });
+        });
+      } else {
+        // if/whenever user sign-out, userAuth is null, set currentUser to vaule of userAuth which is null
+        this.setState({ currentUser: userAuth })
+      }
+    });
   }
 
   // beacuse it is an open subscrition, so we have to close subscriptions when this unmouted to avoid any memory leaks
