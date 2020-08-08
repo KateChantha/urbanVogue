26a9255e0357e967404971e5f0be8457ec29ed05
Kato <kubkate@gmail.com>
2020-08-07 17:49:05 -0700

    Add logic in firebase.utils to store user data from sign-in

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import SignInSignUpPage from './pages/signin-signup-page/signin-signup.component
 // import { auth } from './firebase/firebase.utils';
 
 import './App.css';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 /**
  * store state of user in App
@@ -31,9 +31,11 @@ class App extends React.Component {
   componentDidMount() {
     // this connection always open as long as app mouted on the DOM. so, no need to keep fecthing to check if user status has changed.
     // auth.onAuthStateChanged() returns fucntion store in unsubscribeFromAuth method/property
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
-      console.log(user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      // this.setState({ currentUser: user })
+      // console.log(user)
+      createUserProfileDocument(user);
+
     })
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -7,6 +7,37 @@ const config = {
 
 };
 
+// make an API request
+// save userAuth object and additaionaldata obj to database
+export const createUserProfileDocument = async (userAuth, additonalData) => {
+  // checkk if object is null-user sign out, then exit this fucntion
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  // "get" "snapshot" obj that reference user data
+  const snapShot = await userRef.get();
+
+  // if user not already in the data base, then create it
+  // "set" "document" obj to create user data
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additonalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message)
+    }
+  }
+  // return userRef incase we need to do something with data
+  return userRef;
+}
+
 //initailize firebase
 firebase.initializeApp(config);
 
